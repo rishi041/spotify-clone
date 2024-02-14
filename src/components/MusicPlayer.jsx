e987479f8ec437b35b58901963983019de96c217
Rushikesh Ganorkar <85555891+rishi041@users.noreply.github.com>
2024-02-14 17:01:27 +0530

    Update MusicPlayer.jsx

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
+import PlayIcon from "../assets/play.png";
+import PauseIcon from "../assets/pause.png";
 // import "./musicPlayer.css";
 import { useStateProvider } from "../utils/StateProvider";
 
@@ -9,9 +11,13 @@ export default function MusicPlayer() {
   const songRef = useRef();
   const progressRef = useRef();
   const ctrlRef = useRef();
+  const [currentTimeDuration, setCurrentTimeDuration] = useState({
+    start: 0,
+    end: 30,
+  });
 
   const [data, setData] = useState(selectedPlaylist);
-  console.log(data, "data");
+
   useEffect(() => {
     if (currentPlaying) {
       setData(currentPlaying);
@@ -39,6 +45,11 @@ export default function MusicPlayer() {
       const currentTime = songRef.current.currentTime;
       const duration = songRef.current.duration;
 
+      setCurrentTimeDuration({
+        start: currentTime,
+        end: duration,
+      });
+
       // Update progress bar
       progressRef.current.value = currentTime;
       progressRef.current.max = duration;
@@ -58,7 +69,11 @@ export default function MusicPlayer() {
 
   useEffect(() => {
     const updateControlButton = () => {
-      ctrlRef.current.textContent = isPlaying ? "pause" : "play";
+      ctrlRef.current = isPlaying ? (
+        <img className="pausePlayIcons" src={PauseIcon} alt="pauseicon" />
+      ) : (
+        <img className="pausePlayIcons" src={PlayIcon} alt="playicon" />
+      );
     };
 
     updateControlButton();
@@ -72,26 +87,57 @@ export default function MusicPlayer() {
   if (!token) return null;
   return (
     <Container>
+      <audio ref={songRef} src={data?.preview_url} />
       <div className="musicContainer">
-        <div>music Name: {data?.name} </div>
-        <div>
-          <audio ref={songRef} src={data?.preview_url} />
-
-          <img src={data?.trackImage} />
-          <input
-            type="range"
-            ref={progressRef}
-            value={
-              progressRef?.current?.value === undefined
-                ? "0"
-                : progressRef?.current?.value
-            }
-            onChange={handleProgressBarChange}
-          />
-
-          <div ref={ctrlRef} onClick={playPauseFunc}>
-            {!isPlaying ? "play" : "pause"}
+        <div className="musicInfo">
+          <div className="musicTitle">
+            <img src={data?.trackImage} />
+            <div>{data?.name} </div>
           </div>
+          <div className="musicControls">
+            <div className="progressBar">
+              <div className="progressNo">
+                00:{Math.round(currentTimeDuration?.start)}
+              </div>
+              <div>
+                <input
+                  type="range"
+                  id="music-bar"
+                  ref={progressRef}
+                  value={
+                    progressRef?.current?.value === undefined
+                      ? "0"
+                      : progressRef?.current?.value
+                  }
+                  onChange={handleProgressBarChange}
+                />
+              </div>
+              <div className="progressNo">
+                00:{Math.round(currentTimeDuration?.end)}
+              </div>
+            </div>
+
+            <div ref={ctrlRef} onClick={playPauseFunc}>
+              {isPlaying ? (
+                currentTimeDuration?.start === 0 ? (
+                  <img
+                    className="pausePlayIcons"
+                    src={PlayIcon}
+                    alt="playicon"
+                  />
+                ) : (
+                  <img
+                    className="pausePlayIcons"
+                    src={PauseIcon}
+                    alt="pauseicon"
+                  />
+                )
+              ) : (
+                <img className="pausePlayIcons" src={PlayIcon} alt="playicon" />
+              )}
+            </div>
+          </div>
+          <div className="soundControls"></div>
         </div>
       </div>
     </Container>
@@ -101,6 +147,62 @@ export default function MusicPlayer() {
 const Container = styled.div`
   .musicContainer {
     color: #fff;
-    display: flex;
+    .musicInfo {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      .musicTitle {
+        display: flex;
+        align-items: flex-end;
+      }
+
+      .musicControls {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .progressBar {
+          display: flex;
+          align-items: baseline;
+          .progressNo {
+            padding: 0 10px;
+          }
+        }
+        .pausePlayIcons {
+          margin-top: 0.9rem;
+          width: 40px;
+        }
+      }
+
+      .soundControls {
+        width: 100px;
+      }
+
+      input[type="range"]::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        background: white;
+      }
+
+      input[type="range"] {
+        -webkit-appearance: none;
+        appearance: none;
+        background: #2c2929;
+      }
+
+      input:hover {
+        cursor: pointer;
+      }
+
+      #music-bar {
+        width: 340px;
+        margin-top: 1.5rem;
+        height: 2.5px;
+        border-radius: 0;
+        transition: all 0.3s ease;
+      }
+    }
   }
 `;
