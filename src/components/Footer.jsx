6ff707ea173f0af6f4451312709c613d0d49d0be
Rushikesh Ganorkar <85555891+rishi041@users.noreply.github.com>
2024-02-16 15:01:37 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,56 @@
 import styled from "styled-components";
 // import Player from "./Player";
 import MusicPlayer from "./MusicPlayer";
+import {AiFillGithub,FaLinkedinIn ,AiFillInstagram} from ""
 
 export default function Footer() {
   return (
     <Container>
       <MusicPlayer />
+      <div>
+        <div>
+        <div  className="footer-copywright">
+          <h3>Designed and Developed by Shubham Pawar</h3>
+        </div>
+        <div  className="footer-copywright">
+          <h3>Copyright © {year} SP</h3>
+        </div>
+        <div  className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/shubhampawar7/"
+                style={{ divor: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/shubhampawar-/"
+                style={{ divor: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/shubhampawar__07/"
+                style={{ divor: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </div>
     </Container>
   );
 }
@@ -13,10 +58,10 @@ export default function Footer() {
 const Container = styled.div`
   height: 100%;
   width: 100%;
-  background-color: #181818;
+  background-divor: #181818;
   border-top: 1px solid #282828;
   // display: grid;
-  // grid-template-columns: 1fr 2fr 1fr;
+  // grid-template-divumns: 1fr 2fr 1fr;
   align-items: center;
   justify-content: center;
   padding: 0 1rem;
