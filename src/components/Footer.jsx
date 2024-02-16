ef319e78ffaf9455727ca09728342a470b32421d
Rushikesh Ganorkar <85555891+rishi041@users.noreply.github.com>
2024-02-16 15:34:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,5 +28,5 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   padding: 0 1rem;
-  background: linear-gradient(transparent, rgb(32, 87, 100),rgb(0, 0, 0));
+  background: linear-gradient(rgb(24 64 74),black);
 `;
