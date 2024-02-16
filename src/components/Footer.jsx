2cb05df2cc3367ddc7c655e973c3dd48fe6bd428
Rushikesh Ganorkar <85555891+rishi041@users.noreply.github.com>
2024-02-16 15:11:38 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,13 @@
 import styled from "styled-components";
 // import Player from "./Player";
 import MusicPlayer from "./MusicPlayer";
-import {AiFillGithub, AiFillInstagram} from "react-icons/ai"
-import {FaLinkedinIn} from "react-icons/fa"
+// import {AiFillGithub, AiFillInstagram} from "react-icons/ai"
+// import {FaLinkedinIn} from "react-icons/fa"
 
 export default function Footer() {
   return (
     <Container>
       <MusicPlayer />
-      <div>
-        <div>
-        <div  className="footer-copywright">
-          <h3>Designed and Developed by Rushikesh Ganorkar</h3>
-        </div>
-        <div  className="footer-copywright">
-          <h3>Copyright © {year} SP</h3>
-        </div>
-        <div  className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/rishi041/"
-                style={{ divor: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/rushikesh-ganorkar-rd/"
-                style={{ divor: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/"
-                style={{ divor: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      </div>
     </Container>
   );
 }
