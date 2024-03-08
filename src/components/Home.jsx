524c8e465afb7760f123e8108e4693f94ed43743
Rushikesh Ganorkar <85555891+rishi041@users.noreply.github.com>
2024-03-08 18:41:06 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -114,7 +114,7 @@ const Container = styled.div`
     padding: 1rem 1rem;
     .songContainer {
       display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
       grid-gap: 1rem;
       .songCard {
         color: #fff;
